f76a2ebbe660b2b642a3f2b4207ee6493699428d
soap2k6 <pdsoper@gmail.com>
2015-07-17 13:15:28 -0400

    Wikipedia page opening done

--- scripts.js
+++ scripts.js
@@ -2,7 +2,8 @@
 
 $(document).ready(function() {
 
-  getWikipedia('Delaware');
+  getPage('Delaware');
+  // openPage('7930');
 
 // My API (POST https://en.wikipedia.org/w/api.php)
 
@@ -12,7 +13,7 @@ $(document).ready(function() {
           "action": "query",
           "list": "search",
           "srsearch": searchStr,
-          "srlimit": "10",
+          "srlimit": 10,
           "format": "json",
           "continue": "",
       }),
@@ -33,6 +34,75 @@ $(document).ready(function() {
         /* ... */
     });
   }
+
+// /w/api.php?action=query&list=pageswithprop&format=json&pwppropname=Delaware&pwpprop=title&pwplimit=10&pwpdir=ascending&indexpageids=&titles=Delaware
+  function getPage(title) {
+    $.ajax({
+      url: "https://en.wikipedia.org/w/api.php?" + jQuery.param({
+          "action": "query",
+          "list": "pageswithprop",
+          "pwprop": "title",
+          "pwplimit": 10,
+          "indexpageids": "",
+          "pwppropname": title,
+          "titles": title,
+          "format": "json",
+      }),
+      dataType: "jsonp",
+      type: "POST",
+    })
+    .done(function(data, textStatus, jqXHR) {
+        console.log("HTTP Request Succeeded: " + jqXHR.status);
+        console.log(data);
+        if (data.query.pageids.length > 1) {
+          console.log("Pageids for ", title, " = ", data.query.pageids);
+        }
+        if (data.query.pageids.length > 0) {
+          openPage(data.query.pageids[0]);
+        }
+        $('#json').append(makeJSONTable(data, "Results of getPage"));
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        console.log("HTTP Request Failed");
+        console.log(jqXHR);
+        console.log(errorThrown);
+    })
+    .always(function() {
+        /* ... */
+    });
+
+  }
+
+function openPage(pageid) {
+   $.ajax({
+      url: "https://en.wikipedia.org/w/api.php?" + jQuery.param({
+          "action": "query",
+          "prop": "info",
+          "inprop": "url",
+          "pageids": pageid,
+          "format": "json",
+      }),
+      dataType: "jsonp",
+      type: "POST",
+    })
+    .done(function(data, textStatus, jqXHR) {
+        console.log("HTTP Request Succeeded: " + jqXHR.status);
+        console.log(data);
+        $('#json').append(makeJSONTable(data, "Results of oepnPage"));
+        var url = data.query.pages[pageid].canonicalurl
+        console.log(url);
+        window.open(url);
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        console.log("HTTP Request Failed");
+        console.log(jqXHR);
+        console.log(errorThrown);
+    })
+    .always(function() {
+        /* ... */
+    });
+  }
+
 
   function getAutoComplete(searcgStr) {
     // Per http://www.labnol.org/internet/tools/using-wikipedia-api-demo-source-code-example/3076/
@@ -66,7 +136,7 @@ $(document).ready(function() {
   }
 
   function makeJSONTable(obj, heading) {
-    /* This returns HTML for a nested table of JSON data. 
+    /* This returns HTML for a nested table of JSON data.
     Use Bootstrap, if available.  If not, use css */
     var bootstrap_enabled = (typeof $().modal === 'function');
     var tableBody = "";
@@ -86,7 +156,7 @@ $(document).ready(function() {
         tableBody += '<tr><td>' + k + '</td><td>';
       } else {
         tableBody += '<tr style="background-color:white;color:black;border:1px solid black">'
-                  +  '<td style="background-color:white;color:black;border:1px solid black">' 
+                  +  '<td style="background-color:white;color:black;border:1px solid black">'
                   + k + '</td><td>';
       }
       if (typeof v !== "object") {
@@ -96,7 +166,7 @@ $(document).ready(function() {
       }
     });
     tableBody += '</td></tr>\n</tbody>\n</table>';
-    return tableBody; 
+    return tableBody;
   }
 
 });
